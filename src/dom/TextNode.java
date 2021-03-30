d8bb4b7369efcffd4bb5f826b6c6274b0efc1108
brunozzz <16675056030>
2021-03-31 00:00:36 +0800

    haode

--- src/dom/TextNode.java
+++ src/dom/TextNode.java
@@ -1,20 +1,21 @@
 package dom;
 
 public class TextNode extends Node {
-        String text;
-        public TextNode(String text){
-            super();
-            node_type = "text";
+    String text;
 
-            this.tag_name = "text_";
-            this.text = text;
-        }
-        public TextNode(){
-            super();
-        }
-
-        @Override
-        public String toString() {
-            return text;
-        }
+    public TextNode(String text) {
+        super();
+        node_type = "text";
+        this.tag_name = "text_";
+        this.text = text;
     }
+
+    public TextNode() {
+        super();
+    }
+
+//    @Override
+//    public String toString() {
+//        return text;
+//    }
+}
